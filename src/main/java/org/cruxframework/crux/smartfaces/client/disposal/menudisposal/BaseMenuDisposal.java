ce99ed4468fdc7ef3b8fc402e0c673170130c14e
thiagobustamante <thiago@cruxframework.org>
2015-08-21 15:35:13 -0300

    Issue #787

--- src/main/java/org/cruxframework/crux/smartfaces/client/disposal/menudisposal/BaseMenuDisposal.java
+++ src/main/java/org/cruxframework/crux/smartfaces/client/disposal/menudisposal/BaseMenuDisposal.java
@@ -262,7 +262,7 @@ public abstract class BaseMenuDisposal extends SingleCrawlableViewContainer
 
 					if(!event.getSelectedItem().hasChildren())
 					{
-						disposal.menuPanel.remove(menu);
+						showSmallMenu(disposal);
 					}
 				}
 			});
