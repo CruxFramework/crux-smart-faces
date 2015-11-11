273950e49d64a49c3b465f4714f8f73c807a015f
Samuel Cardoso <samuel.cardoso@triggolabs.com>
2015-11-11 19:40:40 -0200

    IGNORE enabling swapviewcontainer widget

--- src/main/java/org/cruxframework/crux/smartfaces/rebind/swapviewcontainer/SwapViewContainerFactory.java
+++ src/main/java/org/cruxframework/crux/smartfaces/rebind/swapviewcontainer/SwapViewContainerFactory.java
@@ -24,6 +24,7 @@ import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreator;
 import org.cruxframework.crux.core.rebind.screen.widget.WidgetCreatorContext;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.HasViewHandlersFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.creator.children.WidgetChildProcessor;
+import org.cruxframework.crux.core.rebind.screen.widget.declarative.DeclarativeFactory;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttribute;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttributeDeclaration;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagAttributes;
@@ -34,6 +35,8 @@ import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagConstrain
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagEvent;
 import org.cruxframework.crux.core.rebind.screen.widget.declarative.TagEvents;
 import org.cruxframework.crux.core.shared.Experimental;
+import org.cruxframework.crux.smartfaces.client.swappanel.SwapViewContainer;
+import org.cruxframework.crux.smartfaces.rebind.Constants;
 
 class SwapContainerContext extends WidgetCreatorContext
 {
@@ -46,7 +49,7 @@ class SwapContainerContext extends WidgetCreatorContext
  * THIS CLASS IS NOT READY TO BE USED IN PRODUCTION. IT CAN CHANGE FOR NEXT RELEASES
  */
 @Experimental
-//@DeclarativeFactory(id="swapViewContainer", library=Constants.LIBRARY_NAME, targetWidget=SwapViewContainer.class)
+@DeclarativeFactory(id="swapViewContainer", library=Constants.LIBRARY_NAME, targetWidget=SwapViewContainer.class)
 @TagChildren({
 	@TagChild(SwapViewContainerFactory.ViewProcessor.class)
 })
