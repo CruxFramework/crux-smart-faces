f9de2365aa0cc41f40077eb00733a01ea575c38c
Samuel Cardoso <samuel.cardoso@triggolabs.com>
2015-12-11 11:47:23 -0200

    Issue #881 - RollingPanel is throwing an exception when running in a small device

--- src/main/java/org/cruxframework/crux/smartfaces/client/rollingpanel/RollingPanelTouchImpl.java
+++ src/main/java/org/cruxframework/crux/smartfaces/client/rollingpanel/RollingPanelTouchImpl.java
@@ -17,6 +17,8 @@ package org.cruxframework.crux.smartfaces.client.rollingpanel;
 
 import java.util.HashMap;
 
+import org.cruxframework.crux.smartfaces.client.backbone.common.FacesBackboneResourcesCommon;
+
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -33,67 +35,17 @@ class RollingPanelTouchImpl extends ScrollPanel implements RollingPanel.PanelImp
 	static final String DEFAULT_ITEM_STYLE_NAME = "faces-itemRollingPanel";
 	static final String DEFAULT_ITEM_WRAPPER_STYLE_NAME = "faces-itemWrapperRollingPanel";
 	
-	private static class WrappedWidget
-	{
-		protected Widget widget;
-		protected FlowPanel wrappedWidget;
-		protected int index;
-		
-		public WrappedWidget(Widget widget, FlowPanel wrappedWidget, int index)
-		{
-			this.widget = widget;
-			this.index = index;
-			this.wrappedWidget = wrappedWidget;
-		}
-
-		@Override
-		public int hashCode() 
-		{
-			final int prime = 31;
-			int result = 1;
-			result = prime * result
-					+ ((widget == null) ? 0 : widget.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) 
-		{
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			WrappedWidget other = (WrappedWidget) obj;
-			if (widget == null) {
-				if (other.widget != null)
-					return false;
-			} else if (!widget.equals(other.widget))
-				return false;
-			return true;
-		}
-	}
-	
 	private HashMap<Widget,WrappedWidget> items = new HashMap<Widget,WrappedWidget>();
+	
 	private FlowPanel itemsContainer = new FlowPanel();
 	private boolean scrollToAddedWidgets = false;
-
 	public RollingPanelTouchImpl()
     {
 		itemsContainer.setStyleName(DEFAULT_ITEM_WRAPPER_STYLE_NAME);
+		itemsContainer.addStyleName(FacesBackboneResourcesCommon.INSTANCE.css().flexBoxHorizontalContainer());
 		super.add(itemsContainer);
     }
-	
-	private FlowPanel createItem(Widget child)
-	{
-		FlowPanel wrapper = new FlowPanel();
-		wrapper.setStyleName(DEFAULT_ITEM_STYLE_NAME);
-		wrapper.add(child);
-		items.put(child,new WrappedWidget(child, wrapper, itemsContainer.getWidgetCount()));
-		return wrapper;
-	}
-	
+
 	@Override
 	public void add(final Widget child)
 	{
@@ -109,67 +61,13 @@ class RollingPanelTouchImpl extends ScrollPanel implements RollingPanel.PanelImp
 			});
 		}
 	}
-
+	
 	@Override
 	public int getScrollPosition()
 	{
 		return getElement().getScrollLeft();
 	}
-
-	@Override
-	public void insert(final Widget widget, int i)
-    {
-		itemsContainer.insert(createItem(widget), i);
-		if (scrollToAddedWidgets)
-		{
-			Scheduler.get().scheduleDeferred(new ScheduledCommand()
-			{
-				public void execute()
-				{
-					scrollToWidget(widget);
-				}
-			});
-		}
-    }
 	
-	@Override
-	public boolean isScrollToAddedWidgets()
-    {
-    	return scrollToAddedWidgets;
-    }
-
-	@Override
-	public boolean remove(int index)
-    {
-		items.remove(index);
-		return itemsContainer.remove(index);
-    }
-	
-	@Override
-	public boolean remove(Widget w) 
-	{
-		items.remove(w);
-		return itemsContainer.remove(w);
-	}
-	
-	@Override
-	public void scrollToWidget(Widget widget)
-	{
-		ensureVisible(items.get(widget).wrappedWidget);
-	}
-	
-	@Override
-	public void setScrollPosition(int position)
-	{
-		setHorizontalScrollPosition(position);
-	}
-
-	@Override
-	public void setScrollToAddedWidgets(boolean scrollToAddedWidgets)
-    {
-    	this.scrollToAddedWidgets = scrollToAddedWidgets;
-    }
-
 	@Override
 	public Widget getWidget(int index) 
 	{
@@ -191,10 +89,115 @@ class RollingPanelTouchImpl extends ScrollPanel implements RollingPanel.PanelImp
 	{
 		return items.size();
 	}
-
+	
 	@Override
 	public int getWidgetIndex(Widget child) 
 	{
 		return items.get(child).index;
+	}
+	
+	@Override
+	public void insert(final Widget widget, int i)
+    {
+		itemsContainer.insert(createItem(widget), i);
+		if (scrollToAddedWidgets)
+		{
+			Scheduler.get().scheduleDeferred(new ScheduledCommand()
+			{
+				public void execute()
+				{
+					scrollToWidget(widget);
+				}
+			});
+		}
+    }
+
+	@Override
+	public boolean isScrollToAddedWidgets()
+    {
+    	return scrollToAddedWidgets;
+    }
+	
+	@Override
+	public boolean remove(int index)
+    {
+		items.remove(index);
+		return itemsContainer.remove(index);
+    }
+
+	@Override
+	public boolean remove(Widget w) 
+	{
+		items.remove(w);
+		return itemsContainer.remove(w);
+	}
+	
+	@Override
+	public void scrollToWidget(Widget widget)
+	{
+		ensureVisible(items.get(widget).wrappedWidget);
+	}
+	
+	@Override
+	public void setScrollPosition(int position)
+	{
+		setHorizontalScrollPosition(position);
+	}
+	
+	@Override
+	public void setScrollToAddedWidgets(boolean scrollToAddedWidgets)
+    {
+    	this.scrollToAddedWidgets = scrollToAddedWidgets;
+    }
+
+	private FlowPanel createItem(Widget child)
+	{
+		FlowPanel wrapper = new FlowPanel();
+		wrapper.setStyleName(DEFAULT_ITEM_STYLE_NAME);
+		wrapper.add(child);
+		items.put(child,new WrappedWidget(child, wrapper, itemsContainer.getWidgetCount()));
+		return wrapper;
+	}
+
+	private static class WrappedWidget
+	{
+		protected int index;
+		protected Widget widget;
+		protected FlowPanel wrappedWidget;
+		
+		public WrappedWidget(Widget widget, FlowPanel wrappedWidget, int index)
+		{
+			this.widget = widget;
+			this.index = index;
+			this.wrappedWidget = wrappedWidget;
+		}
+
+		@Override
+		public boolean equals(Object obj) 
+		{
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			WrappedWidget other = (WrappedWidget) obj;
+			if (widget == null) {
+				if (other.widget != null)
+					return false;
+			} else if (!widget.equals(other.widget))
+				return false;
+			return true;
+		}
+
+		@Override
+		public int hashCode() 
+		{
+			final int prime = 31;
+			int result = 1;
+			result = prime * result
+					+ ((widget == null) ? 0 : widget.hashCode());
+			return result;
+		}
 	}
 }
